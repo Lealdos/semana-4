dade38639b67affc600977ada783e3d68b1d1c07
jose delgado <j3leonardodelgado@gmail.com>
2023-04-11 22:52:06 -0400

    humanize function, date insert in HTML with JS

---
+++ student-blogs/leal2-6465/main.mjs
@@ -0,0 +1,24 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime.js";
+dayjs.extend(relativeTime);
+
+// Humanize Date Function
+const humanizeDate = (date) => {
+  const now = dayjs();
+  const diff = now.diff(date, "days");
+  if (diff < 30) {
+    return date.fromNow();
+  }
+  return date.year() === now.year()
+    ? date.format("MMMM D")
+    : date.format("MMMM D, YYYY");
+};
+
+// Show Humanize Date in HTML
+let timeTags = document.querySelectorAll("time");
+console.log(timeTags);
+timeTags.forEach((timeTag) => {
+  let date = timeTag.getAttribute("datetime");
+  let convertedDate = humanizeDate(dayjs(date));
+  timeTag.innerText = convertedDate;
+});
